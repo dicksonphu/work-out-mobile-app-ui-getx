3fc1db0f8dd3e1fd4ce87da3eab15b87edde0cc2
Dickson Phu <dicksonphu@gmail.com>
2023-10-10 13:04:48 +0700

    no message

--- lib/bindings/login_binding.dart
+++ lib/bindings/login_binding.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 
 import '../controller/authControllers/login_controller/login_controller.dart';
+import '../controller/authControllers/login_controller/extensions/login_with_account.dart';
 
 class LoginBinding implements Bindings {
   @override
